823143010b55a3fad11338d4e355686c8b616117
Lilian <lilian.kojan@gmail.com>
2021-01-27 13:07:09 +0100

    estimated first iteration of CC model

--- SEM Climate Crisis/model-cc-1.R
+++ SEM Climate Crisis/model-cc-1.R
@@ -1,15 +1,14 @@
 library(tidyverse)
+# library(remotes)
+# remotes::install_github("sem-in-r/seminr", ref = "model-viz", force = TRUE)
 library(seminr)
 
-# Load data and crop to relevant section -c(1:47,97:145)
-databoth <- read_csv("Data/S1-Data-preliminary.csv")[,-c(1:47,97:145)] %>% as.data.frame()
-data <- databoth[!is.na(databoth$CCSKN),] %>% as.matrix()
-data
-# # mean of knowledge
-# datakn <- data %>% transmute(CCKN = mean(c(CCKN1, CCKN2, CCKN2_1, CCKN3, CCKN4, CCKN5, CCKN6)))
+# Load data
+datafull <- read_rds("Data/S1-data-nm.RDS")
+datacrop <- datafull %>% select(starts_with("CC") & !paste0("CCS", 1:4)) %>% filter(!is.na(CCSKN))
+data <- as.matrix(datacrop %>% select(!starts_with("CCKN")) %>% cbind("CCKN" = rowMeans(datacrop %>% select(starts_with("CCKN")))))
 
 # Describe measurement model
-# CCKN1 is placeholder
 mm <- constructs(
   composite("Perceived Self-Efficacy", multi_items("CCRB", 1:3), mode_B),
   composite("Perceived Response Efficacy", multi_items("CCRB", 4:6), mode_B),
@@ -19,13 +18,13 @@ mm <- constructs(
   composite("Competence", multi_items("CCDI", 4:6)),
   composite("Integrity", multi_items("CCDI", 7:9)),
   higher_composite("Distrusting Beliefs", c("Benevolence", "Competence", "Integrity"), mode_B),
-  composite("Knowledge", single_item("CCKN1")),
+  composite("Knowledge", single_item("CCKN")),
   composite("Perceived Susceptibility", multi_items("CCTB", 1:3)),
   composite("Perceived Severity", multi_items("CCTB", 4:6)),
   higher_composite("Threat Beliefs", c("Perceived Susceptibility", "Perceived Severity"), mode_B),
   composite("Personal Moral Norm", multi_items("CCPN", 1:3)),
-  composite("Descriptive Norm", multi_items("CCDN", 1:3), mode_B),
-  composite("Injunctive Norm", multi_items("CCIN", 1:3), mode_B),
+  composite("Descriptive Norm", multi_items("CCDN", 1:2), mode_B),
+  composite("Injunctive Norm", multi_items("CCIN", 1:2), mode_B),
   higher_composite("Subjective Norm", c("Descriptive Norm", "Injunctive Norm"), mode_B),
   composite("Behavioral Intention", multi_items("CCBI", 1:3), mode_B)
 )
@@ -37,10 +36,8 @@ sm <- relationships(
 
 
 model <- estimate_pls(data, mm, sm)
+#plot(model)
 saveRDS(model, "SEM Climate Crisis/model-cc-1.RDS")
-model <- readRDS("SEM Climate Crisis/model-cc-1.RDS")
-summo <- summary(model)
-sumfs <- summary(model$first_stage_model)
+#model <- readRDS("SEM Climate Crisis/model-cc-1.RDS")
 bootmodel <- bootstrap_model(model, nboot = 5000)
 saveRDS(bootmodel, "SEM Climate Crisis/model-boot-cc-1.RDS")
-summary(bootmodel)
