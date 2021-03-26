98687cacc5c66d00d47aa4c46dc0d1de40cfae15
André Calero Valdez <andrecalerovaldez@gmail.com>
2021-03-26 12:20:52 +0100

    implemented way to retrieve encrypted data from private OSF component

---
+++ 00_get_raw_data.R
@@ -0,0 +1,28 @@
+# This script contains code to get the raw data from a private OSF repository.
+
+library(osfr)
+library(tidyverse)
+osf_id <- "rvkx4" # This component will remain private!
+
+osf_node <- osf_retrieve_node(osf_id)
+
+osf_node %>%
+  osf_ls_files() %>%
+  osf_download(here::here("data", "raw"),
+               recurse = TRUE,
+               conflicts = "overwrite",
+               progress = TRUE)
+
+source(here::here("Data", "password.R"))
+
+zip_file <- here::here("Data", "raw", "encrypted_data.zip")
+target_folder <- here::here("Data", "raw")
+
+if(str_detect(sessionInfo()$platform, "darwin")) {
+  # This works on the mac
+  system(paste0(
+    "unzip -P ", password, " ", zip_file, " *.csv -d ", target_folder
+  ))
+}
+
+
