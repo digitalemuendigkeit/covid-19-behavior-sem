0e789e68fc2878748dfe3d5f4754e9f8c78fd438
André Calero Valdez <andrecalerovaldez@gmail.com>
2021-03-26 12:41:59 +0100

    added a download script.

--- Data/10_anonymize_data.R
+++ Data/10_anonymize_data.R
@@ -16,12 +16,12 @@ open_path <- raw_path <- here::here("Data", "open")
 osf_id_open <- "9xzdr"
 osf_id_protected <- "rvkx4" # This component will remain private
 
-
-
+# get osf nodes
 osf_open_node <- osf_retrieve_node(osf_id_open)
 osf_protected_node <- osf_retrieve_node(osf_id_protected)
 
 
+# start upload to osf
 
 files_to_upload <- dir(open_path, pattern = "*.txt", recursive = T)
 
