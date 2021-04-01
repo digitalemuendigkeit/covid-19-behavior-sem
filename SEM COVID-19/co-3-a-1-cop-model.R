59b6b1649f9105a016d4cf79658b701cb8a369f0
Lilian <lilian.kojan@gmail.com>
2021-04-01 12:24:33 +0200

    refactored endogeneity eval

---
+++ SEM COVID-19/co-3-a-1-cop-model.R
@@ -0,0 +1,118 @@
+library(tidyverse)
+library(seminr)
+
+# Data Loading ----
+# Load data and crop to relevant section -c(1:43,48:96,143:145)
+datafull <- read_rds(here::here("Data",
+                                "open",
+                                "S1-data-nm.RDS"))
+
+# Load copula
+RB_star <- readRDS( here::here("SEM COVID-19",
+                              "Models",
+                              "copula-rb-model-co-3-a-1.RDS"))
+# append to data
+data <- datafull %>%
+  select(starts_with("CO")) %>%
+  select(-paste0("COS", 1:7)) %>%
+  filter(!is.na(COSKN)) %>% # other condition
+  mutate(COKN = rowMeans(across(starts_with("COKN")))) %>%
+  cbind(RB_star)
+
+
+
+# Model Description ----
+
+# Describe measurement model
+# Inclusion of Response Beliefs copula
+
+mm <- constructs(
+  composite("Perceived Response Efficacy", multi_items("CORB", 4:6), mode_B),
+  composite("Perceived Response Costs", multi_items("CORB", 7:9), mode_B),
+  higher_composite("Response Beliefs", c("Perceived Response Efficacy", "Perceived Response Costs"), weights =  mode_B),
+  composite("Benevolence", multi_items("CODI", 1:3)),
+  composite("Competence", multi_items("CODI", 4:6)),
+  composite("Integrity", multi_items("CODI", 7:9)),
+  higher_composite("Distrusting Beliefs", c("Benevolence", "Competence", "Integrity"), weights = mode_B),
+  composite("Knowledge", single_item("COKN")),
+  composite("Personal Moral Norm", multi_items("COPN", c(1,3))),
+  composite("Descriptive Norm", multi_items("CODN", 1:2), mode_B),
+  composite("Behavioral Intention", multi_items("COBI", 1:3), mode_B),
+  composite("Copula Response Beliefs", single_item("RB_star"))
+)
+
+sm <- relationships(
+  paths(from = c("Distrusting Beliefs", "Knowledge"), to = c("Response Beliefs")),
+  paths(from = c("Response Beliefs", "Personal Moral Norm", "Descriptive Norm", "Copula Response Beliefs"), to = "Behavioral Intention")
+)
+
+# Model Estimation ----
+
+model <- estimate_pls(data, mm, sm)
+saveRDS(model, "SEM COVID-19/Models/model-co-3-a-1-cop.RDS")
+bootmodel <- bootstrap_model(model, nboot = 5000)
+saveRDS(bootmodel, "SEM COVID-19/Models/model-boot-co-3-a-1-cop.RDS")
+bootfsmodel <- bootstrap_model(model$first_stage_model, nboot = 5000)
+saveRDS(bootfsmodel, "SEM COVID-19/Models/model-fs-boot-co-3-a-1-cop.RDS")
+
+# Proxy Model ----
+
+# Make proxy model for PLSpredict
+proxymm <- constructs(
+  composite("Response Beliefs", multi_items("CORB", c(4:9)), weights = mode_B),
+  composite("Distrusting Beliefs", multi_items("CODI", 1:9), weights = mode_B),
+  composite("Knowledge", single_item("COKN")),
+  composite("Personal Moral Norm", multi_items("COPN", c(1,3))),
+  composite("Descriptive Norm", multi_items("CODN", 1:2), weights = mode_B),
+  composite("Behavioral Intention", multi_items("COBI", 1:3), mode_B)
+)
+
+proxymodel <- estimate_pls(as.data.frame(data), proxymm, sm)
+saveRDS(proxymodel, "SEM COVID-19/Models/model-proxy-co-3-a-1-cop.RDS")
+
+# # Convergent Validity ----
+#
+# # Models for estimation of convergent validity for formative constructs
+# # Perceived Response Efficacy
+# mmpre <- constructs(
+#   composite("Perceived Response Efficacy Formative", multi_items("CORB", 4:6), mode_B),
+#   composite("Perceived Response Efficacy Reflective", single_item("CORB11"))
+# )
+# smpre <- relationships(
+#   paths(from = "Perceived Response Efficacy Formative", to = "Perceived Response Efficacy Reflective")
+# )
+# rapre <- estimate_pls(data, mmpre, smpre)
+# saveRDS(rapre, "SEM COVID-19/Models/rapre-co-3-a-1-cop.RDS")
+#
+# # Perceived Response Costs
+# mmprc <- constructs(
+#   composite("Perceived Response Costs Formative", multi_items("CORB", 7:9), mode_B),
+#   composite("Perceived Response Costs Reflective", single_item("CORB12"))
+# )
+# smprc <- relationships(
+#   paths(from = "Perceived Response Costs Formative", to = "Perceived Response Costs Reflective")
+# )
+# raprc <- estimate_pls(data, mmprc, smprc)
+# saveRDS(raprc, "SEM COVID-19/Models/raprc-co-3-a-1-cop.RDS")
+#
+# # Descriptive Norm
+# mmdn <- constructs(
+#   composite("Descriptive Norm Formative", multi_items("CODN", 1:2), mode_B),
+#   composite("Descriptive Norm Reflective", single_item("CODN4"))
+# )
+# smdn <- relationships(
+#   paths(from = "Descriptive Norm Formative", to = "Descriptive Norm Reflective")
+# )
+# radn <- estimate_pls(data, mmdn, smdn)
+# saveRDS(radn, "SEM COVID-19/Models/radn-co-3-a-1-cop.RDS")
+#
+# # Behavioral Intention
+# mmbi <- constructs(
+#   composite("Behavioral Intention Formative", multi_items("COBI", 1:3), mode_B),
+#   composite("Behavioral Intention Reflective", single_item("COBI4"))
+# )
+# smbi <- relationships(
+#   paths(from = "Behavioral Intention Formative", to = "Behavioral Intention Reflective")
+# )
+# rabi <- estimate_pls(data, mmbi, smbi)
+# saveRDS(rabi, "SEM COVID-19/Models/rabi-co-3-a-1-cop.RDS")
